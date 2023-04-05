bf60b150e87b152322f89b0cc472ab91676a6f03
Anthony Iszer <aiszler29@gmail.com>
2023-04-05 15:29:56 -0700

    adding more topics

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::io;
 // just some practice on general functions and data type info.
 
 fn main() {
-
-      let y = {
-        let x = 3;
-        x + 1
+  let mut y = {  //sets var for i32 "y"
+        let x = 3; // gives x a value
+        x + 1 // adds 1    
     };
+    
 
     let z =five();
     println!("{z}");
@@ -24,6 +24,32 @@ fn main() {
     let number: i32 = number.trim().parse().expect("please use a nunber");
 
     another_function(number);
+
+
+    let num = 3;
+
+    if num != 3 {               // the number is NOT (!=) to 3
+        println!("the number is 3!");
+    }
+    else if num == 2 {          // this wont trigger because the above statement is true
+        println!("looks like its 2?")
+    }
+    else {                      // in case eveerything was true before, this will trigger
+        println!("the number isnt 3");   
+    }
+
+
+    let num2 = 12;
+    if num2 % 5 == 0 {                           // false so no trigger
+         println!("the number is divisible by 4")
+    }   
+   else if num2 % 3 == 0 {                       // true so it will trigger
+       println!("the number is divisible by 3")
+   }
+   else {                                       // wont trigger because the above statement is true
+       println!("the number us {}", num2)
+   } 
+
 }
 
 fn another_function(x: i32) {
@@ -33,3 +59,4 @@ fn another_function(x: i32) {
 fn five() -> i32 {
     5
 }
+
